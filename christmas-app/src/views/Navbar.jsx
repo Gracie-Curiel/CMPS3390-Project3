cfb9f62aff7e1371ecca20203344886c9d65a0d7
oscar <orocha@csub.edu>
2025-12-07 16:53:02 -0800

    fixed navbar directory

--- christmas-app/src/views/Navbar.jsx
+++ christmas-app/src/views/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "../styles/Navbar.css";
 export default function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
